ef506ce05f8c158eb86af08146b2654ec0029b53
shadedcolors <allarounduseful@gmail.com>
2023-07-26 22:52:27 -0500

    Update 0.1.2 -Shadow Blink Ability

--- Shadow Swipe/Assets/Scripts/PlayerController.cs
+++ Shadow Swipe/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,23 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     //Reference to Game Manager
     private GameManager gameManager;
 
-    //Get Player and Wall layers
-    private int layerMask;
+    //Player Collider
+    private CircleCollider2D playerCollider;
+
+    //Get layers
+    private int layerMaskPlayer;
+    private int layerMaskShadowDetector;
+    private int layerMaskWall;
+
+    //Shadow Detector Prefab
+    public GameObject shadowDetector;
 
     //Basic Player Movement
     public float baseSpeed;
@@ -20,16 +29,28 @@ public class PlayerController : MonoBehaviour
     private Vector2 targetPosition;
     private Rigidbody2D rb;
 
+    // Shadow Blink
+    public float tapSpeed;
+    private float lastTapTime;
+    public float ShadowBlinkCastTime;
+
+    //Double Tap Detection for Shadow Blink
+
     private void Awake()
     {
         //Get reference to Game Manager
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
-        //Get Player and Wall layers
-        layerMask = LayerMask.GetMask("Player", "Wall");
+        // Get Layer Masks
+        layerMaskPlayer = LayerMask.GetMask("Player", "Wall");
+        layerMaskShadowDetector = LayerMask.GetMask("Wall", "ShadowDetector");
+        layerMaskWall = LayerMask.GetMask("Wall");
 
         //Get rigidbody for movement
         rb = GetComponent<Rigidbody2D>();
+
+        //Get Player Collider
+        playerCollider = GetComponent<CircleCollider2D>();
     }
 
     private void Start()
@@ -41,8 +62,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckForDoubleTap();
+
         //Check if player is in shadow
-        if (IsPlayerInShadow())
+        if (IsInShadow(gameObject))
         {
             Debug.Log("Player is in shadow");
         }
@@ -62,26 +85,48 @@ public class PlayerController : MonoBehaviour
     }
 
     //Iterates through all light sources and determines if the player is in shadow
-    bool IsPlayerInShadow()
+    bool IsInShadow(GameObject target)
     {
-        // Iterate through all light sources
+        //go through all light sources
         foreach (GameObject lightSource in gameManager.lightSources)
         {
-            // Cast a ray from the light source to the player
-            Vector2 rayDirection = transform.position - lightSource.transform.position;
-            float lightRange = lightSource.GetComponent<Light2D>().pointLightOuterRadius; // get the Light2D component
+            // Cast a ray from the light source to the target
+            Vector2 rayDirection = target.transform.position - lightSource.transform.position;
+            float lightRange = lightSource.GetComponent<Light2D>().pointLightOuterRadius;
 
-            RaycastHit2D hit = Physics2D.Raycast(lightSource.transform.position, rayDirection, lightRange, layerMask);
-
-            // Check if the raycast hit the player before it hit a wall
-            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            if (target == gameObject) // Player
             {
-                // The raycast hit the player before it hit a wall, so the player is in light
-                return false;
+                // Use raycast with layerMaskPlayer
+                RaycastHit2D hit = Physics2D.Raycast(lightSource.transform.position, rayDirection, lightRange, layerMaskPlayer);
+
+                // If the raycast hit something, check if it hit the target
+                if (hit.collider != null)
+                {
+                    // If the raycast hit the player, the player is not in shadow
+                    if (hit.collider.tag == "Player")
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (target == shadowDetector) // Shadow detector
+            {
+                // Use raycast with layerMaskShadowDetector
+                RaycastHit2D hit = Physics2D.Raycast(lightSource.transform.position, rayDirection, lightRange, layerMaskShadowDetector);
+
+                // If the raycast hit something, check if it hit the target
+                if (hit.collider != null)
+                {
+                    // If the raycast hit the shadow detector, the the shadow blink position is not in shadow
+                    if (hit.collider.tag == "ShadowDetector")
+                    {
+                        return false;
+                    }
+                }
             }
         }
 
-        // The raycast hit a wall before it hit the player for all light sources, so the player is in shadow
+        // The raycast hit a wall before it hit the target for all light sources, or it did not hit anything (i.e., it's out of range), so the target is in shadow
         return true;
     }
 
@@ -131,5 +176,87 @@ public class PlayerController : MonoBehaviour
 
         // Update the player's position
         rb.MovePosition(movePosition);
+    }
+
+    public void CheckForDoubleTap()
+    {
+        //Mouse Input
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // Check if the time between taps is less than the tap speed
+            if (Time.time - lastTapTime < tapSpeed)
+            {
+                // Double Tap
+                StartCoroutine(DoubleTapped(Input.mousePosition));
+            }
+
+            //Reset last tap time
+            lastTapTime = Time.time;
+        }
+
+        // Touch Input
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            // Check if the time between taps is less than the tap speed
+            if (Time.time - lastTapTime < tapSpeed)
+            {
+                // Double Tap
+                StartCoroutine(DoubleTapped(Input.GetTouch(0).position));
+            }
+
+            //Reset last tap time
+            lastTapTime = Time.time;
+        }
+    }
+
+    public IEnumerator DoubleTapped(Vector2 screenPosition)
+    {
+        // Convert screen position to world position
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        // Check if the player's sprite overlaps with a wall
+        Collider2D hitCollider = Physics2D.OverlapCircle(worldPosition, playerCollider.radius / 2, layerMaskWall);
+
+        if (hitCollider != null && hitCollider.gameObject.tag == "Wall")
+        {
+            // If the hit collider's gameobject is tagged as "Wall", the point is inside a wall
+            Debug.Log("Point is inside a wall");
+            yield break;
+        }
+        else
+        {
+            // The point is not inside a wall
+            Debug.Log("Point is not inside a wall");
+        }
+
+        // Cast a ray from the player to the desired blink position
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, worldPosition - (Vector2)transform.position, Vector2.Distance(transform.position, worldPosition));
+
+        // Go through all the objects hit by the raycast
+        foreach (RaycastHit2D hit in hits)
+        {
+            // If we hit a wall, we cannot blink
+            if (hit.collider.gameObject.tag == "Wall")
+            {
+                Debug.Log("Can't blink here: wall in the way");
+                yield break;
+            }
+        }
+
+        shadowDetector.transform.position = worldPosition;
+
+        // Wait for a duration before checking IsInShadow
+        yield return new WaitForSeconds(ShadowBlinkCastTime);
+
+        if (IsInShadow(shadowDetector))
+        {
+            // Use Shadow Blink
+            Debug.Log("Blink successful");
+            transform.position = worldPosition;
+
+            // Reset the last tap time after successful blink
+            lastTapTime = 0;
+        }
     }
 }
---
+++ Shadow Swipe/Assets/Scripts/ShadowDetector.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowDetector : MonoBehaviour
+{
+    public Vector2 position;
+
+    public void SetPosition(Vector2 position)
+    {
+        this.position = position;
+    }
+}
